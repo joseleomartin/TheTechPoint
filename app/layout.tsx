398d436ae9867eda148d52f20da8d8601c502413
leonardo <leojosemartin@gmail.com>
2025-11-16 18:12:36 -0300

    fix: Cambiar Analytics para funcionar con export estÃ¡tico

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { WhatsAppButton } from '@/components/WhatsAppButton';
 
 const inter = Inter({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
